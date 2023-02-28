b16ea2fd21e4f5e4dd9253005373ded495176b95
trofimovmax <92295615+TrofimovMax@users.noreply.github.com>
2023-02-28 18:29:23 +0300

    feat: base app layout

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import Heading from './Heading';
+const Footer = () => (
+    <>
+        <footer>
+            <Heading tag='h6' text='created by ' />
+        </footer>
+    </>
+);
+export default Footer;
---
+++ src/pages/404/index.js
@@ -0,0 +1,9 @@
+import Heading from '/src/components/Heading'
+const Error = () => (
+    <>
+        <Heading text='Error 404'/>
+        <Heading tag='h2' text='Page not found...'/>
+    </>
+
+);
+export default Error;
