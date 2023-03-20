3610b9159a28e112bfa55bbca785f99ba3784767
trofimovmax <92295615+TrofimovMax@users.noreply.github.com>
2023-03-20 14:43:25 +0300

    feat: style slider item component

--- src/components/CarouselItem.js
+++ src/components/CarouselItem.js
@@ -1,49 +1,76 @@
 import React from 'react';
-import {Grid, Button, Typography} from '@mui/material'
+import {Grid, Button, Typography, createTheme, ThemeProvider} from '@mui/material'
+import CssBaseline from "@mui/material/CssBaseline";
+
+const theme = createTheme({
+  palette: {
+    button: "#ff6479",
+    '&:hover': {
+      button_hover: "#f4576c",
+    }
+  },
+});
+
 const CarouselItem = ({item}) => {
 
   const styles = {
     paperContainer: {
       backgroundImage: `url(${item.image})`
-    }
+    },
+    button: {
+      marginTop: 30,
+      padding: "18px 36px",
+      fontSize: "18px",
+      color: "#FFFFFF"
+    },
   };
 
   return (
-    <Grid container spacing={3} style={styles.paperContainer} sx={
-      {
-        width: 1,
-        minHeight: '50vw'
-      }
-    }>
-      <Grid item xs={12} sx={{
-        marginTop: '25vw',
-        marginLeft: '5vw'
-      }}>
-        <Grid item >
-          <Typography component='h4' color="common.white">
-            {item.title}
-          </Typography>
-        </Grid>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Grid container spacing={3} style={styles.paperContainer} sx={
+        {
+          width: 1,
+          minHeight: '50vw'
+        }
+      }>
+        <Grid container spacing={5}>
+          <Grid item xs={12} sx={{
+            marginTop: '15vh',
+            marginLeft: '5vw'
+          }}>
+            <Grid item xs={4}>
+              <Typography variant="h4"
+                          component="h5"
+                          color="common.white">
+                {item.title}
+              </Typography>
+            </Grid>
 
-        <Grid item >
-          <Typography component='h6' sx={{maxWidth:"20vw"}} color="common.white">
-            {item.text}
-          </Typography>
-        </Grid>
+            <Grid item xs={6}>
+              <Typography variant="h5" component="h6"
+                          sx={{maxWidth: "20vw", marginTop: 3,}}
+                          color="common.white">
+                {item.text}
+              </Typography>
+            </Grid>
 
-        <Grid item >
-          <Button className="CheckButton"
-                  style={{
-                    backgroundColor: "#D22B2B",
-                    padding: "18px 36px",
-                    fontSize: "18px",
-                    color: "#FFFFFF"
-                  }}>
-            GET STARTED
-          </Button>
+            <Grid item xs={4}>
+              <Button
+                sx={{
+                  backgroundColor: 'button',
+                  '&:hover': {
+                    backgroundColor: '&:hover.button_hover'
+                  },
+                }}
+                style={styles.button}>
+                GET STARTED
+              </Button>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
-    </Grid>
+    </ThemeProvider>
   );
 };
 
