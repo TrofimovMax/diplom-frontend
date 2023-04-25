418f9c61ca102ec4e9968f55189dad666ffa47bc
trofimovmax <92295615+TrofimovMax@users.noreply.github.com>
2023-04-25 12:07:24 +0300

    feat: add Dialog for BookingForm

--- src/components/templates/gyms/organisms/BookingForm/index.js
+++ src/components/templates/gyms/organisms/BookingForm/index.js
@@ -1,7 +1,16 @@
 import React, {useContext, useState} from "react";
-import {Box, ThemeProvider, createTheme, Typography} from "@mui/material";
-import { useMutation } from "react-query";
-import { createDataTimeUTC } from "@/components/templates/gyms/utils";
+import {
+  Box,
+  ThemeProvider,
+  createTheme,
+  Typography,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions, Button, DialogContentText
+} from "@mui/material";
+import {useMutation} from "react-query";
+import {createDataTimeUTC} from "@/components/templates/gyms/utils";
 import axiosClient from "@/api/axiosClient";
 import NoticeContext from "@/api/NoticeContext";
 
@@ -26,17 +35,29 @@ const theme = createTheme({
 });
 
 const BookingForm = ({gymId, time, date, capacity, count, refetchBookings}) => {
-  const [counter, setCouter] = useState(count);
+  const [counter, setCounter] = useState(count);
+  const [open, setOpen] = React.useState(false);
+
+  const handleDialogOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDialogClose = () => {
+    setOpen(false);
+  };
+
   const start_at = (time - 1) + ':00';
   const end_at = time + ':00';
+
   const {handleClick, setResponseMessage, setSeverity} = useContext(NoticeContext);
-  const { data, mutate } = useMutation(["create_bookings"], (params) => {
+
+  const {data, mutate} = useMutation(["create_bookings"], (params) => {
       axiosClient.post(`/gyms/${gymId}/bookings`, params)
     },
     {
       onSuccess: (response) => {
         refetchBookings()
-        setCouter(counter + 1);
+        setCounter(counter + 1);
         handleClick();
         setResponseMessage("Your successfully crated your booking!");
         setSeverity("success");
@@ -50,6 +71,7 @@ const BookingForm = ({gymId, time, date, capacity, count, refetchBookings}) => {
   );
 
   const booking = (gymId, start_at, end_at, date) => () => {
+    handleDialogClose();
     mutate({
       start_at: createDataTimeUTC(date, start_at),
       end_at: createDataTimeUTC(date, end_at),
@@ -73,7 +95,7 @@ const BookingForm = ({gymId, time, date, capacity, count, refetchBookings}) => {
   return (
     <ThemeProvider theme={theme}>
       <Box
-        onClick={booking(gymId, start_at, end_at, date)}
+        onClick={handleDialogOpen}
         sx={{
           height: 1,
           width: 1,
@@ -85,6 +107,30 @@ const BookingForm = ({gymId, time, date, capacity, count, refetchBookings}) => {
         }}>
         <Typography variant="caption">{counter} / {capacity}</Typography>
       </Box>
+      <Dialog
+        open={open}
+        onClose={handleDialogClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {`Do you want to book from ${start_at} to ${end_at} on ${date}?`}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Book or add the time when you would like to come to the lesson and we will take it into account when scheduling
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDialogClose}>Cancel</Button>
+          <Button onClick={handleDialogClose}>
+            Add wish list
+          </Button>
+          <Button onClick={booking(gymId, start_at, end_at, date)} autoFocus>
+            Book
+          </Button>
+        </DialogActions>
+      </Dialog>
     </ThemeProvider>
   );
 }
