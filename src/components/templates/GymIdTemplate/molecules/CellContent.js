d3a1ddb9d5b3021cc7d0a474cbc1d9e828533d46
trofimovmax <92295615+TrofimovMax@users.noreply.github.com>
2023-04-26 17:42:48 +0300

    refactor: unite WishForm and BookingForm in CellForm

--- src/components/templates/GymIdTemplate/molecules/CellContent.js
+++ src/components/templates/GymIdTemplate/molecules/CellContent.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {hasHourInSchedule} from "@/components/templates/GymIdTemplate/organisms/GymTableTemplate/utils";
 import {StyledTableCell} from "@/components/templates/GymIdTemplate/organisms/GymTableTemplate/styles";
-import BookingForm from "@/components/templates/GymIdTemplate/organisms/BookingForm";
 import moment from "moment/moment";
 import filter from "lodash/filter";
-import WishingForm from "@/components/templates/GymIdTemplate/organisms/WishingForm";
+import {CellForm} from "@/components/templates/GymIdTemplate/organisms/CellForm";
 
 const CellContent = ({gymId, day, hour, capacity, bookings, refetch, schedule}) => {
+  const isOpenGymByHour = hasHourInSchedule(day, hour, schedule);
   const preparedBookings = bookings.map((item) => {
     return {
       ...item,
@@ -19,32 +19,20 @@ const CellContent = ({gymId, day, hour, capacity, bookings, refetch, schedule}) 
     const bookingsByTime = filter(preparedBookings, { start })
     return bookingsByTime.length
   }
-  if(hasHourInSchedule(day, hour, schedule)){
-    return (
-      <StyledTableCell sx={{border: 1, padding: 0, width: 70, height: 70}} key={hour}
-                       component="th" scope="row">
-        <BookingForm date={day.substring(4)}
-                     time={hour}
-                     gymId={gymId}
-                     capacity={capacity}
-                     count={getBookingsCountByTime(bookings, day, hour-1)}
-                     refetchBookings={refetch}
-        />
-      </StyledTableCell>
-    )
-  } else {
-    return (
-      <StyledTableCell sx={{border: 1, padding: 0, width: 70, height: 70}} key={hour} component="th"
-                       scope="row">
-        <WishingForm
-          date={day.substring(4)}
-          time={hour}
-          gymId={gymId}
-          refetchBookings={refetch}
-        />
-      </StyledTableCell>
-    )
-  }
+
+  return(
+    <StyledTableCell sx={{border: 1, padding: 0, width: 70, height: 70}} key={hour}
+                     component="th" scope="row">
+      <CellForm date={day.substring(4)}
+                time={hour}
+                gymId={gymId}
+                capacity={capacity}
+                count={getBookingsCountByTime(bookings, day, hour-1)}
+                refetchBookings={refetch}
+                isOpenGymByHour = {isOpenGymByHour}
+      />
+    </StyledTableCell>
+  )
 };
 
 export default CellContent;
---
+++ src/components/templates/GymIdTemplate/organisms/CellForm/index.js
@@ -0,0 +1,192 @@
+import React, {useContext, useState} from 'react';
+import {
+  Button,
+  createTheme,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Grid, ThemeProvider,
+  Typography
+} from "@mui/material";
+import NoticeContext from "@/api/NoticeContext";
+import {useMutation} from "react-query";
+import axiosClient from "@/api/axiosClient";
+import {createDataTimeUTC} from "@/components/templates/GymIdTemplate/utils";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#00A36C'
+    },
+    secondary: {
+      main: '#7CFC00'
+    },
+    yellow: {
+      main: '#FFEA00'
+    },
+    orange: {
+      main: '#FFBF00'
+    },
+    red: {
+      main: '#FF0000'
+    },
+    white: {
+      main: '#FFFFFF',
+      secondary: '#e5e5e5',
+    }
+  },
+});
+export const CellForm = ({date,time, gymId,capacity,count,refetchBookings,isOpenGymByHour}) => {
+  const [counter, setCounter] = useState(count);
+  const [open, setOpen] = React.useState(false);
+
+  const start_at = (time - 1) + ':00';
+  const end_at = time + ':00';
+
+  const dialogTitle = isOpenGymByHour ? `Do you want to book from ${start_at} to ${end_at} on ${date}?`:
+    `Do you want to add in wish list from ${start_at} to ${end_at} on ${date}?`;
+  const dialogContentText = isOpenGymByHour ? 'some book':
+    'Add the time when you would like to come to the lesson and we will take it into account when scheduling';
+  const submitButtonText = isOpenGymByHour ? "Book": "Add wish list";
+
+  const countBooking = isOpenGymByHour ? <Typography paddingLeft={2} variant="caption">{counter} / {capacity}</Typography>:
+    null;
+  const handleDialogOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDialogClose = () => {
+    setOpen(false);
+  };
+
+  const {handleClick, setResponseMessage, setSeverity} = useContext(NoticeContext);
+
+  const { mutate } = useMutation(["create_bookings"], (params) => {
+      axiosClient.post(`/gyms/${gymId}/bookings`, params)
+    },
+    {
+      onSuccess: (response) => {
+        refetchBookings()
+        setCounter(counter + 1);
+        handleClick();
+        setResponseMessage("Your successfully created your booking!");
+        setSeverity("success");
+      },
+      onError: (error) => {
+        handleClick();
+        setResponseMessage(error?.message);
+        setSeverity("error");
+      }
+    }
+  );
+  const booking = (gymId, start_at, end_at, date) => () => {
+    handleDialogClose();
+    mutate({
+      start_at: createDataTimeUTC(date, start_at),
+      end_at: createDataTimeUTC(date, end_at),
+    })
+  };
+
+  const { mutateAsync } = useMutation(["create_wishes"], (params) => {
+      axiosClient.post(`/gyms/${gymId}/wishes`, params)
+    },
+    {
+      onSuccess: (response) => {
+        handleClick();
+        setResponseMessage("Your successfully created your wish in list!");
+        setSeverity("success");
+      },
+      onError: (error) => {
+        handleClick();
+        setResponseMessage(error?.message);
+        setSeverity("error");
+      }
+    }
+  );
+
+  const wishing = (gymId, start_at, end_at, date) => () => {
+    handleDialogClose();
+    mutateAsync({
+      start_at: createDataTimeUTC(date, start_at),
+      end_at: createDataTimeUTC(date, end_at),
+    })
+  };
+
+  const submitButtonOnClick = isOpenGymByHour ? booking(gymId, start_at, end_at, date):
+    wishing(gymId, start_at, end_at, date);
+  const initMainColor = () =>{
+    if (count >= capacity) {
+      return 'red.main';
+    } else if (count >= capacity * 0.75) {
+      return 'orange.main';
+    } else if (count >= capacity * 0.5) {
+      return 'yellow.main';
+    } else {
+      return 'primary.main';
+    }
+  }
+  const initSecondaryColor = () =>{
+    if (count >= capacity) {
+      return 'red.main';
+    } else if (count >= capacity * 0.75) {
+      return 'orange.main';
+    } else if (count >= capacity * 0.5) {
+      return 'yellow.main';
+    } else {
+      return 'secondary.main';
+    }
+  }
+
+  const activeBackgroundColor = isOpenGymByHour ? initMainColor(): 'white.main';
+  const secondaryBackgroundColor = isOpenGymByHour ? initSecondaryColor(): 'white.secondary';
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        onClick={handleDialogOpen}
+        sx={{
+          height: 1,
+          width: 1,
+          backgroundColor: activeBackgroundColor,
+          '&:hover': {
+            backgroundColor: secondaryBackgroundColor,
+            opacity: [0.9, 0.8, 0.7],
+          },
+        }}
+        spacing={0}
+      >
+        <Grid item xs>
+          {countBooking}
+        </Grid>
+      </Grid>
+      <Dialog
+        open={open}
+        onClose={handleDialogClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {dialogTitle}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {dialogContentText}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="outlined" color="error" onClick={handleDialogClose}>Cancel</Button>
+          <Button variant="contained" color="success" autoFocus onClick={submitButtonOnClick}>
+            {submitButtonText}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </ThemeProvider>
+  );
+};
+
