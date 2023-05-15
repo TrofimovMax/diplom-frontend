3268a632688ff83dd9af4d1705e3112847802e9d
trofimovmax <92295615+TrofimovMax@users.noreply.github.com>
2023-05-15 18:00:34 +0300

    feat: add googlemaps and little red pointer

--- src/components/templates/AboutUsTemplate/index.js
+++ src/components/templates/AboutUsTemplate/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import {Container, Grid, Typography} from "@mui/material";
+import {Container, Grid} from "@mui/material";
 import TitleSection from "@/components/molecules/TitleSection";
-import RoomOutlinedIcon from "@mui/icons-material/RoomOutlined";
-import PhoneAndroidOutlinedIcon from "@mui/icons-material/PhoneAndroidOutlined";
-import MailOutlineOutlinedIcon from "@mui/icons-material/MailOutlineOutlined";
-import MessageOutlinedIcon from '@mui/icons-material/MessageOutlined';
-import TelegramIcon from "@mui/icons-material/Telegram";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import EmailIcon from "@mui/icons-material/Email";
-import LinkAtom from "@/components/atoms/LinkAtom";
-
+import Contacts from "@/components/molecules/Contacts";
+import GoogleMapReact from 'google-map-react';
+import PlaceIcon from '@mui/icons-material/Place';
+import {red} from "@mui/material/colors";
 const AboutUsTemplate = () => {
+  const defaultProps = {
+    center: {
+      lat: 47.21908057615701,
+      lng: 38.91923206870906
+    },
+    zoom: 15
+  };
+
   return (
     <Container>
       <Grid container spacing={{xs: 1, md: 2}}>
@@ -19,91 +22,25 @@ const AboutUsTemplate = () => {
           <TitleSection title="Контакты"/>
         </Grid>
 
-        <Grid container item>
-          <Grid container item xs={12} spacing={2}
-                direction="row"
-                justifyContent="center"
-                alignItems="center">
-            <Grid item>
-              <RoomOutlinedIcon/>
-            </Grid>
-            <Grid item>
-              <Typography variant="h6" component="h3">Адресс</Typography>
-              <Typography variant="body1"> Главный офис: Петровская 51 </Typography>
-            </Grid>
-          </Grid>
-          <Grid container item
-                sx={{mr: 7}}
-                xs={12}
-                direction="row"
-                justifyContent="center"
-                alignItems="center">
-            <Grid item>
-              <PhoneAndroidOutlinedIcon sx={{mt: 2}}/>
-            </Grid>
-            <Grid item xs={2} sx={{ml: 2}}>
-              <Typography variant="h6" component="h3">Телефоны</Typography>
-              <Typography variant="body1"> +1 (409) 987–5874 </Typography>
-              <Typography variant="body1"> +1 (409) 987–5874 </Typography>
-            </Grid>
-          </Grid>
-          <Grid container item
-                sx={{mr: 8}}
-                xs={12}
-                spacing={2}
-                direction="row"
-                justifyContent="center"
-                alignItems="center">
-            <Grid item>
-              <MailOutlineOutlinedIcon/>
-            </Grid>
-            <Grid item xs={2}>
-              <Typography variant="h6" component="h3">Почта</Typography>
-              <Typography variant="body1"> sport.palace@gmail.com </Typography>
-            </Grid>
-          </Grid>
-          <Grid container item
-                sx={{mr: 8}}
-                xs={12}
-                spacing={2}
-                direction="row"
-                justifyContent="center"
-                alignItems="center">
-            <Grid item>
-              <MessageOutlinedIcon/>
-            </Grid>
-            <Grid container item xs={2}>
-              <Grid item>
-                <Typography variant="h6" component="h3" sx={{mr: 2}}>Наши соцсети</Typography>
-              </Grid>
-              <Grid item>
-                <LinkAtom
-                  children={<TelegramIcon/>}
-                  href={"https://web.telegram.org/"}
-                  color={"redLinks"}
-                  collor_hover={"&:hover.redLinks_hover"}
-                />
-              </Grid>
-              <Grid item>
-                <LinkAtom
-                  children={<InstagramIcon/>}
-                  href={"https://www.instagram.com/"}
-                  color={"redLinks"}
-                  color_hover={"&:hover.redLinks_hover"}
-                />
-              </Grid>
-              <Grid item>
-                <LinkAtom
-                  children={<EmailIcon/>}
-                  href={"https://www.google.com/intl/ru/gmail/about/"}
-                  color={"redLinks"}
-                  color_hover={"&:hover.redLinks_hover"}
-                />
-              </Grid>
-            </Grid>
-          </Grid>
+        <Contacts/>
+
+        <Grid item sx={{
+          height:'100vh',
+          width: '100%'
+        }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: "" }}
+            defaultCenter={defaultProps.center}
+            defaultZoom={defaultProps.zoom}
+          >
+            <PlaceIcon
+              sx={{color: red[500]}}
+              lat={47.21232421421452}
+              lng={38.93384416870883}
+            />
+          </GoogleMapReact>
         </Grid>
-        
+
       </Grid>
     </Container>
   );
