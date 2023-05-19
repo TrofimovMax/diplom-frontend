53c7d43f32dbafa18bc58ef12eefcafc16fa4342
trofimovmax <92295615+TrofimovMax@users.noreply.github.com>
2023-05-19 15:10:43 +0300

    fix: change google map to yandex map

--- src/components/templates/AboutUsTemplate/index.js
+++ src/components/templates/AboutUsTemplate/index.js
@@ -2,18 +2,13 @@ import React from 'react';
 import {Container, Grid} from "@mui/material";
 import TitleSection from "@/components/molecules/TitleSection";
 import Contacts from "@/components/molecules/Contacts";
-import GoogleMapReact from 'google-map-react';
-import PlaceIcon from '@mui/icons-material/Place';
-import {red} from "@mui/material/colors";
-const AboutUsTemplate = () => {
-  const defaultProps = {
-    center: {
-      lat: 47.21908057615701,
-      lng: 38.91923206870906
-    },
-    zoom: 15
-  };
+import {Placemark, YMaps, Map} from "@pbe/react-yandex-maps";
 
+const AboutUsTemplate = () => {
+  const defaultState = {
+    center: [47.212382, 38.933865],
+    zoom: 14,
+  };
   return (
     <Container>
       <Grid container spacing={{xs: 1, md: 2}}>
@@ -28,17 +23,12 @@ const AboutUsTemplate = () => {
           height:'100vh',
           width: '100%'
         }}>
-          <GoogleMapReact
-            bootstrapURLKeys={{ key: "" }}
-            defaultCenter={defaultProps.center}
-            defaultZoom={defaultProps.zoom}
-          >
-            <PlaceIcon
-              sx={{color: red[500]}}
-              lat={47.21232421421452}
-              lng={38.93384416870883}
-            />
-          </GoogleMapReact>
+          {/* docs for using this map: https://pbe-react-yandex-maps.vercel.app/ */}
+          <YMaps>
+            <Map defaultState={defaultState} width='100%' height='100%'>
+              <Placemark geometry={[47.212169, 38.933865]} />
+            </Map>
+          </YMaps>
         </Grid>
 
       </Grid>
